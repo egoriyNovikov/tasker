c719d69025c1a5771812dca24d54e4d262809631
Egor Novikov <egoriyNovikov@gmail.com>
2026-09-01 21:26:49 +0300

    switch to bearer

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\LoginRequest;
-
+use App\Http\Requests\RegisterRequest;
 use App\Services\AuthService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function __construct(private AuthService $authService)
-    {
-    }
+    public function __construct(private AuthService $authService) {}
 
     public function register(RegisterRequest $request)
     {
@@ -26,7 +24,7 @@ class AuthController extends Controller
         ], 201);
     }
 
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         $user = $this->authService->login(
             $request->validated()
@@ -35,11 +33,15 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'User logged in successfully',
             'id' => $user->id,
+            'access_token' => $user->createToken('api-token')->plainTextToken,
+            'token_type' => 'Bearer',
         ], 200);
     }
 
-    public function logout(Request $request)
-    {
+    public function logout(Request $request) {}
 
+    public function me(Request $request): JsonResponse
+    {
+        return response()->json($request->user());
     }
 }
